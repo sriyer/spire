283adebe3fee7c021fed941bb78f09a2a30ef4d1
Sudarshan R Iyer <shan.vr@gmail.com>
2022-09-15 14:05:01 -0400

    added a few more test cases for node attestor
    Signed-off-by: Sudarshan R Iyer <shan.vr@gmail.com>

--- pkg/server/plugin/nodeattestor/kerberos/kerberos_test.go
+++ pkg/server/plugin/nodeattestor/kerberos/kerberos_test.go
@@ -2,6 +2,7 @@ package kerberos
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	nodeattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/server/nodeattestor/v1"
@@ -27,9 +28,12 @@ func (m *mockKerberos) LoadKeyTab() {
 func (m *mockKerberos) LoadConfig() {
 
 }
-func (m *mockKerberos) VerifyAPRequest(kt *gokrbkeytab.Keytab, attestedData *common.KrbAttestedData) (bool, *credentials.Credentials, error) {
+func (m *mockKerberos) VerifyAPRequest(kt *gokrbkeytab.Keytab, attestedData *common.KrbAttestedData) (valid bool, creds *credentials.Credentials, err error) {
 	args := m.Called(kt, attestedData)
-	return args.Bool(0), args.Get(1).(*credentials.Credentials), args.Error(2)
+	if args.Get(1) != nil {
+		creds = args.Get(1).(*credentials.Credentials)
+	}
+	return args.Bool(0), creds, args.Error(2)
 }
 
 func Test_PluginAttest(t *testing.T) {
@@ -40,10 +44,11 @@ func Test_PluginAttest(t *testing.T) {
 	err := kt.Unmarshal(b)
 	assert.Nil(t, err, "Unable to unmarshal test keytab data")
 
-	mk := &mockKerberos{}
-	p := &Plugin{Kerberos: mk, keytab: kt, trustDomain: "example.com"}
+	
 
 	t.Run("should attest node", func(t *testing.T) {
+		mk := &mockKerberos{}
+		p := &Plugin{Kerberos: mk, keytab: kt, trustDomain: "example.com"}
 		attestedData := &common.KrbAttestedData{}
 		payload, err := json.Marshal(attestedData)
 		assert.Nil(t, err)
@@ -61,8 +66,50 @@ func Test_PluginAttest(t *testing.T) {
 
 		t.Logf("id - %s, selectors - %s", id, selectors)
 	})
+	t.Run("should handle attestation failure", func(t *testing.T) {
+		mk := &mockKerberos{}
+		p := &Plugin{Kerberos: mk, keytab: kt, trustDomain: "example.com"}
+		attestedData := &common.KrbAttestedData{}
+		payload, err := json.Marshal(attestedData)
+		assert.Nil(t, err)
+		req := &nodeattestorv1.AttestRequest{Request: &nodeattestorv1.AttestRequest_Payload{Payload: payload}}
+		c := &credentials.Credentials{}
+		c = c.WithKeytab(kt)
+		c.SetDomain("testing")
+		c.SetDisplayName("node.testing.com")
+		mk.On("VerifyAPRequest", kt, attestedData).Return(false, nil, fmt.Errorf("error attesting node"))
+		c.SetCName(types.NewPrincipalName(0, "node.testing.com"))
+		id, selectors, err := p.attestRequest(req)
+		assert.NotNil(t, err)
+		assert.Empty(t, id)
+		assert.Empty(t, selectors)
+
+		t.Logf("id - %s, selectors - %s", id, selectors)
+	})
+	t.Run("should handle validation failure", func(t *testing.T) {
+		mk := &mockKerberos{}
+		p := &Plugin{Kerberos: mk, keytab: kt, trustDomain: "example.com"}
+		attestedData := &common.KrbAttestedData{}
+		payload, err := json.Marshal(attestedData)
+		assert.Nil(t, err)
+		req := &nodeattestorv1.AttestRequest{Request: &nodeattestorv1.AttestRequest_Payload{Payload: payload}}
+		c := &credentials.Credentials{}
+		c = c.WithKeytab(kt)
+		c.SetDomain("testing")
+		c.SetDisplayName("node.testing.com")
+		mk.On("VerifyAPRequest", kt, attestedData).Return(false, nil, nil)
+		c.SetCName(types.NewPrincipalName(0, "node.testing.com"))
+		id, selectors, err := p.attestRequest(req)
+		assert.NotNil(t, err)
+		assert.Empty(t, id)
+		assert.Empty(t, selectors)
+
+		t.Logf("id - %s, selectors - %s", id, selectors)
+	})
 
 	t.Run("cannot parse attest data", func(t *testing.T) {
+		mk := &mockKerberos{}
+		p := &Plugin{Kerberos: mk, keytab: kt, trustDomain: "example.com"}
 		req := &nodeattestorv1.AttestRequest{Request: &nodeattestorv1.AttestRequest_Payload{Payload: []byte("cannot parse this")}}
 		id, selectors, err := p.attestRequest(req)
 		assert.NotNil(t, err)
